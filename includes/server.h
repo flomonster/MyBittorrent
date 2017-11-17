db37b3f763d3e97b8842696e870b11c637a804ed
Florian Amsallem <amsall_f@epita.fr>
2017-11-17 21:34:58 +0100

    server_init: add first implementation

--- includes/server.h
+++ includes/server.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <stdbool.h>
+
+
 typedef struct server
 {
   // boilerplate
   int port;
 } s_server;
+
+
+bool *server_init(s_server *server);
---
+++ src/server.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <err.h>
+#include <stdlib.h>
+
+#include "server.h"
+
+
+bool *server_init(s_server *server)
+{
+  server->port = 0;
+
+  return false;
+}
