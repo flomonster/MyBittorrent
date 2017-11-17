ada7c6dd3c21664cc613f44bd7f9e6832f3846b1
Victor "multun" Collod <victor.collod@epita.fr>
2017-11-17 19:36:55 +0100

    bittorrent: print the metainfo file

--- src/bittorrent.c
+++ src/bittorrent.c
@@ -2,14 +2,25 @@
 #include <stdlib.h>
 
 #include "btopts.h"
+#include "fmap.h"
 
 
-int main (int argc, char **argv)
+int main(int argc, char *argv[])
 {
   int torstart = btopts_parse(argc, argv);
   if (torstart < 0)
     return EXIT_FAILURE;
 
-  puts("ok");
+  for (int i = torstart; i < argc; i++)
+  {
+    struct dbuf *cur_tor = fmap(argv[i]);
+    s_bdata *benc_tor = bencode_parse(cur_tor);
+
+    bdata_print(stdout, benc_tor);
+
+    bencode_free(benc_tor);
+    funmap(cur_tor);
+  }
+
   return EXIT_SUCCESS;
 }
