6a582cdfa7b20433fe73e48f3355ad138bc791f3
Florian Amsallem <amsall_f@epita.fr>
2017-11-18 17:11:19 +0100

    file: add first implementation

--- includes/file.h
+++ includes/file.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <stddef.h>
+
+#include "path.h"
+
+
 typedef struct file
 {
-  char *path;
-  void *addr;
+  s_path *path;
+  void *data;
   size_t size;
   size_t offset;
 } s_file;
+
+
+bool file_init(s_file *file, s_path *path, size_t size, size_t offset);
---
+++ src/file.c
@@ -0,0 +1,16 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "file.h"
+#include "path.h"
+
+
+bool file_init(s_file *file, s_path *path, size_t size, size_t offset)
+{
+  file->size = size;
+  file->offset = offset;
+  file->path = path;
+  file->data = path_map(path, size, AT_FDCWD);
+  return file->data == NULL;
+}
