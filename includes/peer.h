7fe2d1a058e8ef03bc103b9ee85dc1620620e747
Florian Amsallem <amsall_f@epita.fr>
2017-11-18 20:06:45 +0100

    peer: add first implementation of initializer

--- includes/peer.h
+++ includes/peer.h
@@ -1,15 +1,21 @@
 #pragma once
 
-#include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+
+typedef char t_peer_id[20];
 
 typedef struct peer
 {
-  struct peer *next;
   size_t pieces;
   bool interested;
   bool choked;
   bool choking;
   struct sockaddr_in addr;
+  t_peer_id peer_id;
 } s_peer;
+
+
+bool peer_init(s_peer *peer, void *data);
---
+++ src/peer.c
@@ -0,0 +1,27 @@
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <sys/socket.h>
+
+#include "peer.h"
+
+
+bool peer_init(s_peer *peer, void *data)
+{
+  peer->pieces = 0;
+  peer->interested = false;
+  peer->choked = false;
+  peer->choking = false;
+
+  peer->addr.sin_family = AF_INET;
+
+  uint32_t *nip = data;
+  peer->addr.sin_addr.s_addr = ntohl(*nip);
+
+  char *tmp = data;
+  tmp += 4;
+  data = tmp;
+  uint16_t *nport = data;
+  peer->addr.sin_port = ntohs(*nport);
+
+  return false;
+}
