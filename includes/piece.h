0782e267e5d5ddd22820fb2a3bcff6e6049598c5
Victor "multun" Collod <victor.collod@epita.fr>
2017-11-19 16:06:43 +0100

    piece: incrementaly allocate blocks

--- includes/piece.h
+++ includes/piece.h
@@ -2,11 +2,23 @@
 
 #include "filelist.h"
 
+
+typedef enum piece_state
+{
+  PIECE_MISSING,
+  PIECE_FETCHING,
+  PIECE_AVAILABLE,
+} e_piece_state;
+
+
 typedef struct piece
 {
   s_filelist *filelist;
-  bool state;
-  size_t dirty;
-  void *blkmap;
-  void *buf;
+  e_piece_state state;
+
+  // total piece size
+  size_t size;
+
+  // number of retrieved blocks
+  size_t block_done;
 } s_piece;
