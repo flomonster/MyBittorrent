88aa1116dd4e7a4c6b8c4159dced310622803534
Victor "multun" Collod <victor.collod@epita.fr>
2017-11-23 18:47:41 +0100

    bitset: fix the internal bit ordering

--- src/bitset.c
+++ src/bitset.c
@@ -21,16 +21,19 @@ s_bitset *bitset_create(size_t size, char *data)
 }
 
 
+#define BIT_OFF(I) (CHAR_BIT - 1 - (I) % CHAR_BIT)
+#define BYTE_OFF(I) ((I) / CHAR_BIT)
+
 bool bitset_get(s_bitset *bset, size_t i)
 {
-  return bset->data[i / CHAR_BIT] & 1 << (i % CHAR_BIT);
+  return bset->data[BYTE_OFF(i)] & 1 << BIT_OFF(i);
 }
 
 
 void bitset_set(s_bitset *bset, size_t i, bool val)
 {
-  unsigned char *obyte = &bset->data[i / CHAR_BIT];
-  *obyte = (*obyte & ~(1 << i)) | val << i;
+  unsigned char *obyte = &bset->data[BYTE_OFF(i)];
+  *obyte = (*obyte & ~(1 << BIT_OFF(i))) | val << BIT_OFF(i);
 }
 
 
