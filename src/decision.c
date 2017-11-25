262b1566270e587e2ec22f1669d6be699060e27d
Victor "multun" Collod <victor.collod@epita.fr>
2017-11-25 15:53:26 +0100

    decision: remove VLA

--- src/decision.c
+++ src/decision.c
@@ -5,24 +5,21 @@
 #include "piece.h"
 #include "torrent.h"
 
+#include <stdlib.h>
+
 
 size_t select_piece(s_peer *peer, s_torrent *tor)
 {
-  size_t counter[tor->nbpieces];
-  for (size_t i = 0; i < tor->nbpieces; i++)
-    counter[i] = 0;
+  size_t *counter = calloc(sizeof(size_t), tor->nbpieces);
 
   s_peer *cur_peer = tor->peerlist.peers;
-  while (cur_peer)
-  {
+  for (; cur_peer; cur_peer = cur_peer->next)
     for (size_t i = 0; i < tor->nbpieces; i++)
       if (bitset_get(cur_peer->pieces, i))
         counter[i]++;
-    cur_peer = cur_peer->next;
-  }
 
   size_t best = 0;
-  size_t score = UINT_MAX;
+  size_t score = SIZE_MAX;
   for (size_t i = 0; i < tor->nbpieces; i++)
     if (bitset_get(peer->pieces, i)
         && tor->pieces[i].state == PIECE_MISSING
@@ -31,6 +28,8 @@ size_t select_piece(s_peer *peer, s_torrent *tor)
       best = i;
       score = counter[i];
     }
+
+  free(counter);
 
   return best;
 }
