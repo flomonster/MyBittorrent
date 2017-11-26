402367c45a3b9c0a599e6b9f6fc944baedb000a8
Victor "multun" Collod <victor.collod@epita.fr>
2017-11-26 11:26:35 +0100

    handshake: log exchanges

--- src/handshake.c
+++ src/handshake.c
@@ -7,6 +7,7 @@
 
 
 #include <string.h>
+#include <stdlib.h>
 
 
 void handshake_send(struct torrent *tor, struct peer_conn *conn,
@@ -18,6 +19,13 @@ void handshake_send(struct torrent *tor, struct peer_conn *conn,
   memset(hs->reserved, 0, 8);
   memcpy(hs->info_hash, tor->metainfo.sha, 20);
   memcpy(hs->peer_id, tor->peer_id, 20);
+
+  if (btlog_active(L_SNETDBG))
+  {
+    char *pf = peer_format(conn->peer);
+    LOG(L_SNETDBG, "msg: send", tor, "%s: handshake", pf);
+    free(pf);
+  }
 
   trans_setup(trans, decision_send, hs, sizeof(s_bthandshake));
 }
@@ -38,7 +46,12 @@ static t_trans_status handshake_receive_full(struct torrent *tor,
     // TODO: close the socket when a callback returns CLOSING
     return TRANS_CLOSING;
   }
-  LOG(L_NETDBG, "handshake", tor, "full handshake received");
+  if (btlog_active(L_SNETDBG))
+  {
+    char *pf = peer_format(conn->peer);
+    LOG(L_SNETDBG, "msg: recv", tor, "%s: handshake", pf);
+    free(pf);
+  }
   // TODO: actual callback action
   trans_setup(trans, receive_message, &hs->pstr, sizeof(s_bthandshake) - 1);
   return TRANS_DONE;
